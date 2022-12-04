dfa22f64d42936845415f3203bf40138b02592df
antoni boryna <borynaantoni@gmail.com>
2022-12-04 23:53:47 +0100

    day 3

--- day_3/task_1.py
+++ day_3/task_1.py
@@ -1,4 +1,27 @@
-with open("day_3/resources/input_1.txt") as fptr:
+from collections import defaultdict
+
+with open("day_3/resources/input_2.txt") as fptr:
     input_ = list(line.strip() for line in fptr.readlines())
 
-print(input_)
+
+def get_priority(chr_: chr) -> int:
+    if chr_.islower():
+        return ord(chr_) - ord('a') + 1
+    return ord(chr_) - ord('A') + 27
+
+priorities = 0
+for rucksack in input_:
+    items_count = len(rucksack)
+    first_comp = rucksack[:items_count // 2]
+
+    sack_items = {}
+    for item in first_comp:
+        sack_items[item] = True
+
+    second_comp = rucksack[items_count // 2:]
+    for item in second_comp:
+        if item in sack_items:
+            priorities += get_priority(item)
+            break
+
+print(priorities)
--- day_3/task_2.py
+++ day_3/task_2.py
@@ -1,4 +1,38 @@
+from collections import defaultdict
+
+
 with open("day_3/resources/input_2.txt") as fptr:
     input_ = list(line.strip() for line in fptr.readlines())
 
-print(input_)
+    groups = []
+    for idx in range(0, len(input_) // 3):
+        groups.append([])
+        for i in range(3):
+            groups[idx].append(input_[idx * 3 + i])
+
+def get_priority(chr_: chr) -> int:
+    if chr_.islower():
+        return ord(chr_) - ord('a') + 1
+    return ord(chr_) - ord('A') + 27
+
+def scan_group(group: list[str]):
+    get_default = lambda: {
+        0: False,
+        1: False,
+        2: False,
+    }
+
+    items = defaultdict(get_default)
+
+    for idx, rucksack in enumerate(group):
+        for item in rucksack:
+            items[item][idx] = True
+            if all(items[item].values()):
+                return get_priority(item)
+    return 0
+
+
+priorities_sum = 0
+for group in groups:
+    priorities_sum += scan_group(group)
+print(priorities_sum)
