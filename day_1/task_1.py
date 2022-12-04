5c3e3140cb9d55677e5a611883ae34a551689204
antoni boryna <borynaantoni@gmail.com>
2022-12-04 20:12:50 +0100

    day 1

--- day_1/task_1.py
+++ day_1/task_1.py
@@ -1,4 +1,15 @@
-with open("day_1/resources/input_1.txt") as fptr:
+with open("day_1/resources/input_2.txt") as fptr:
     input_ = list(line.strip() for line in fptr.readlines())
 
-print(input_)
+
+cumulative = 0
+calorie_packs = []
+for line in input_:
+    if len(line) < 1:
+        calorie_packs.append(cumulative)
+        cumulative = 0
+        continue
+
+    cumulative += int(line)
+
+print(max(calorie_packs))
