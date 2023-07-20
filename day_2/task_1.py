b841d6390efa216e2f022f93c41dc6ce14c0cbb1
Emilia <voxelost@proton.me>
2023-07-20 14:27:35 +0200

    day 2

--- day_2/task_1.py
+++ day_2/task_1.py
@@ -1,4 +1,47 @@
-with open("day_2/resources/input_1.txt") as fptr:
-    input_ = list(line.strip() for line in fptr.readlines())
+import enum
 
-print(input_)
+with open("day_2/resources/input_2.txt") as fptr:
+    input_ = list(line.strip().split(' ') for line in fptr.readlines())
+
+
+class Result(enum.Enum):
+    Lost = 0
+    Draft = 3
+    Win = 6
+
+
+class AliasDict(dict):
+    def __init__(self, *args, **kwargs):
+        dict.__init__(self, *args, **kwargs)
+        self.aliases = {}
+
+    def __getitem__(self, key):
+        return dict.__getitem__(self, self.aliases.get(key, key))
+
+    def __setitem__(self, keys, value):
+        if len(keys) > 1:
+            for key in keys[1:]:
+                self.aliases[key] = keys[0]
+        return dict.__setitem__(self, self.aliases.get(key, key), value)
+
+
+def battle_result(shape_enemy: str, shape_ally: str) -> Result:
+    if SHAPES[shape_enemy] == SHAPES[shape_ally]:
+        return Result.Draft
+
+    if (SHAPES[shape_enemy] - 1 + 4) % 3 == SHAPES[shape_ally] - 1:
+        return Result.Win
+
+    return Result.Lost
+
+def count_points(shape_enemy: str, shape_ally: str) -> int:
+    return battle_result(shape_enemy, shape_ally).value + SHAPES[shape_ally]
+
+
+SHAPES = AliasDict()
+SHAPES['A', 'X'] = 1
+SHAPES['B', 'Y'] = 2
+SHAPES['C', 'Z'] = 3
+
+print(sum(count_points(*battle) for battle in input_))
+
--- day_2/task_2.py
+++ day_2/task_2.py
@@ -1,4 +1,26 @@
 with open("day_2/resources/input_2.txt") as fptr:
-    input_ = list(line.strip() for line in fptr.readlines())
+    input_ = list(line.strip().split(' ') for line in fptr.readlines())
 
-print(input_)
+
+SHAPES = {
+    'A': 1,
+    'B': 2,
+    'C': 3,
+}
+
+RESULTS = {
+    'X': 0,
+    'Y': 3,
+    'Z': 6,
+}
+
+def count_points(shape_enemy: str, ally_move: str) -> int:
+    if ally_move == 'X':
+        return (SHAPES[shape_enemy] + 1) % 3 + 1 + RESULTS[ally_move] # lose
+    if ally_move == 'Z':
+        return (SHAPES[shape_enemy] + 3) % 3 + 1 + RESULTS[ally_move]  # win
+    return SHAPES[shape_enemy] + RESULTS[ally_move]
+
+
+print(sum(count_points(*battle) for battle in input_))
+
