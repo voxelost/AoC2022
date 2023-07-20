3800269a53ebb51c5813de4b790f6786436db501
Emilia <voxelost@proton.me>
2023-07-20 14:27:35 +0200

    init

---
+++ day_10/task_2.py
@@ -0,0 +1,4 @@
+with open("day_10/resources/input_2.txt") as fptr:
+    input_ = list(line.strip() for line in fptr.readlines())
+
+print(input_)
---
+++ day_24/task_2.py
@@ -0,0 +1,4 @@
+with open("day_24/resources/input_2.txt") as fptr:
+    input_ = list(line.strip() for line in fptr.readlines())
+
+print(input_)
